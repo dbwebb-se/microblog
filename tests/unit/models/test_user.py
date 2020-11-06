e9dce1fd97ee4ac9653aa0d5453ae23ff603a7a6
Alexander Granhof <alexander.g737@gmail.com>
2020-11-06 15:55:35 +0100

    enable tests again

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -2,7 +2,7 @@
 Contains tests for app.models.User class
 """
 # pylint: disable=redefined-outer-name
-# from unittest import mock
+from unittest import mock
 import pytest
 from app.models import User
 
@@ -27,20 +27,20 @@ def test_new_user(user1):
     assert user1.about_me == 'Hello'
     assert str(user1) == "<User john, john@example.com>"
 
-# @mock.patch("app.models.current_app")
-# def test_password_hashing(_mock_current_app, user1):
-#     """
-#     Test setting password for user
-#     """
-#     user1.set_password('cat')
-#     assert user1.check_password('dog') is False
-#     assert user1.check_password('cat') is True
+@mock.patch("app.models.current_app")
+def test_password_hashing(_mock_current_app, user1):
+    """
+    Test setting password for user
+    """
+    user1.set_password('cat')
+    assert user1.check_password('dog') is False
+    assert user1.check_password('cat') is True
 
-# @mock.patch("app.models.current_app")
-# def test_avatar(_mock_current_app, user1):
-#     """
-#     Test creation of Gravatar URL
-#     """
-#     assert user1.avatar(128) == ('https://www.gravatar.com/avatar/'
-#                                  'd4c74594d841139328695756648b6bd6'
-#                                  '?d=retro&s=128')
+@mock.patch("app.models.current_app")
+def test_avatar(_mock_current_app, user1):
+    """
+    Test creation of Gravatar URL
+    """
+    assert user1.avatar(128) == ('https://www.gravatar.com/avatar/'
+                                 'd4c74594d841139328695756648b6bd6'
+                                 '?d=retro&s=128')
