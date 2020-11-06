d959432c0cf103406de09983d4f36c8f657d4a8a
Alexander Granhof <alexander.g737@gmail.com>
2020-11-06 14:51:58 +0100

    try remove test_app

--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -2,7 +2,7 @@
 Contains tests for app.models.User class
 """
 # pylint: disable=redefined-outer-name
-from unittest import mock
+# from unittest import mock
 import pytest
 from app.models import User
 
@@ -28,7 +28,7 @@ def test_new_user(user1):
     assert str(user1) == "<User john, john@example.com>"
 
 # @mock.patch("app.models.current_app")
-def test_password_hashing(test_app, user1):
+def test_password_hashing(user1):
     """
     Test setting password for user
     """
@@ -37,7 +37,7 @@ def test_password_hashing(test_app, user1):
     assert user1.check_password('cat') is True
 
 # @mock.patch("app.models.current_app")
-def test_avatar(test_app, user1):
+def test_avatar(user1):
     """
     Test creation of Gravatar URL
     """
