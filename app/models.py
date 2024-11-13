a2da84118b1bfa8f1b5ff1b0ac4555a8d3f34e15
WeirdnessUnfolds <simon.langstrom@hotmail.se>
2024-11-13 12:16:35 +0100

    docs - Add docstring to followed_posts method

--- app/models.py
+++ app/models.py
@@ -84,6 +84,10 @@ class User(UserMixin, db.Model):
             followers.c.followed_id == user.id).count() > 0
     
     def followed_posts(self):
+        """
+        Returns a query that returns all posts that the user is following,
+        including posts made by the user themselves.
+        """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id) 
