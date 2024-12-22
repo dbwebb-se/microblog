5bb4b00cc3731d4d816fafc1da45bf8dc994f28d
Simon Långström <70513298+WeirdnessUnfolds@users.noreply.github.com>
2024-12-22 14:28:30 +0100

    fix - changed import order

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,9 +1,9 @@
 """
 Contains routes used for authenticating User
 """
+from urllib.parse import urlparse
 from flask import render_template, redirect, url_for, flash, request, current_app
 from flask_login import login_user, logout_user, current_user
-from urllib.parse import urlparse
 from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm
