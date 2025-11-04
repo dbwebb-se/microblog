d9ae2212ca2e25cddd142a4d749ead3dee223dc2
Iwinter78 <kille1404.so@gmail.com>
2025-11-04 14:27:10 +0100

    chore: created starup script for test container

---
+++ boot_dev.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source .venv/bin/activate
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
+exec flask run --host=0.0.0.0
